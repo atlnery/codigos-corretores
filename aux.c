a0f012a72be1b9366625f9c6f577342206f57bcf
Amanda Lopes <49240604+atlnery@users.noreply.github.com>
2020-09-23 04:36:17 -0300

    Add files via upload

--- aux.c
+++ aux.c
@@ -26,3 +26,33 @@ int* new(int n) {
     return new;
 }
 
+int * random_vector_unique_elems(int n) {
+    int* v = new(n);
+
+    for (int i = 0; i < n; i++) {
+        v[i] = i;
+    }
+
+    shuffle(v, 0, n);
+
+    return v;
+}
+
+
+void shuffle(int *v, int ini, int fim) {
+
+    int i, j;
+
+    for(i = fim-1; i > ini; i--){
+        j = (rand() % (i + 1));
+        j = j <= ini? ini+1 : j;
+        swap(v, i, j);
+    }
+}
+
+void swap(int* v, int i, int j) {
+    int temp = v[i]; 
+    v[i] = v[j];
+    v[j] = temp;
+}
+
--- aux.h
+++ aux.h
@@ -8,5 +8,11 @@
     void print(int* v, int n);
     int* copy(char * m);
     int* new(int n);
+    int * random_vector_unique_elems(int n);
+    void shuffle(int *v, int ini, int fim);
+    void swap(int* v, int i, int j);
+
+    
+
     
 #endif
